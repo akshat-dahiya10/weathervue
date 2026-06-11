0f4cfe891a2649b75c2e407f1314b5e3465e2f81
akshat-dahiya10 <dahiyaakshat10@gmail.com>
2026-06-11 11:21:04 +0530

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,8 +4,8 @@ import { Pool } from "pg";
 // const databaseUrl = process.env.DATABASE_URL;
 
 // if (!databaseUrl) {
-  throw new Error("DATABASE_URL is required");
-}
+// throw new Error("DATABASE_URL is required");
+// }
 
 const globalForDb = globalThis as typeof globalThis & {
   __arenaNextJsPostgresqlPool?: Pool;
