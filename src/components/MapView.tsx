235179bf99bb6b4b806aa9cc3ed3e3908a72799b
akshat-dahiya10 <dahiyaakshat10@gmail.com>
2026-06-13 10:02:50 +0530

    Update MapView.tsx

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -13,12 +13,11 @@ export default function MapView({ lat, lon }: MapViewProps) {
 
   return (
     <div className="mt-10 rounded-2xl overflow-hidden border border-white/10">
-      <MapContainer
-        center={position as any}
-        zoom={10}
-        scrollWheelZoom={false}
-        style={{ height: "400px", width: "100%" }}
-      >
+     <MapContainer
+  center={position as unknown as [number, number]}
+  zoom={10}
+  scrollWheelZoom={false}
+>
         <TileLayer
           attribution="© OpenStreetMap"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
